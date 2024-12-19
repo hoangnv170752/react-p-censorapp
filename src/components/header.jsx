53f3a1935cddb7ea7ce9811049380791f145c0e1
hoangnv170752 <hoang.nv.ral@gmail.com>
2024-12-20 00:51:11 +0700

    chore: style change + video

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import eyeIcon from '../assets/eye-white.svg';
+import './navigation.css';
 
 export const Header = (props) => {
   return (
@@ -8,7 +10,12 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
+                <h1 className="text-black">
+                  <img
+                    src={eyeIcon}
+                    alt="Eye Icon"
+                    className="eye-icon-header"
+                  />
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
@@ -19,6 +26,18 @@ export const Header = (props) => {
                 >
                   Learn More
                 </a>{" "}
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  controls={true}
+                  className="video-background"
+                  style={{ marginTop: 100}}
+                >
+                  <source src={require("../assets/intro.mp4")} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
               </div>
             </div>
           </div>
